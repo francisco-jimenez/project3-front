6e44c8e4e4e1326f2390f8a4eea157da43b2056d
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-07-25 16:14:11 +0200

    cambios locos

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,13 +3,18 @@ import background from "../allyouneed.png"
 import patata from "../patata.jpg"
 import { Link } from "react-router-dom";
 import videoBack from "../"
+import { AuthContext } from "./../context/auth.context"
+import { useContext } from "react";
 
 function HomePage() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   return (
     <div style={{ backgroundImage: `url(${patata})` , width: "95rem" , height: "100rem" , marginTop: "0", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-      
+          <span>Hola, {user.name}</span>
+          <br>
+          </br>
       <Link to="/tinder">
-              <button>¿No das match con tu media naranja? Hazlo con tu comida favorita!</button>
+              <button className="intro">¿No das match con tu media naranja? Hazlo con tu comida favorita!</button>
             </Link>
     </div>
   );
