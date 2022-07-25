07191be76813ce4f1949e7390f05f8ec023bf7aa
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-07-25 16:27:20 +0200

    despetado

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,18 +3,13 @@ import background from "../allyouneed.png"
 import patata from "../patata.jpg"
 import { Link } from "react-router-dom";
 import videoBack from "../"
-import { AuthContext } from "./../context/auth.context"
-import { useContext } from "react";
 
 function HomePage() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   return (
     <div style={{ backgroundImage: `url(${patata})` , width: "95rem" , height: "100rem" , marginTop: "0", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-          <span>Hola, {user.name}</span>
-          <br>
-          </br>
+      
       <Link to="/tinder">
-              <button className="intro">¿No das match con tu media naranja? Hazlo con tu comida favorita!</button>
+              <button>¿No das match con tu media naranja? Hazlo con tu comida favorita!</button>
             </Link>
     </div>
   );
