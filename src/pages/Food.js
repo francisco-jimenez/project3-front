c7c6b457e9090a5944577290a23385c6dc96ac72
Nico M <nico_16bcn@hotmail.com>
2022-07-28 20:01:46 +0200

    swipe visual

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -6,6 +6,13 @@ import TinderCard from 'react-tinder-card'
 
 import flechas from "../flechas.png"
 
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+import check from "../check.png"
+import close from "../close.png"
+import arrowleft from "../left-arrow.png"
+import arrowright from "../right-arrow.png"
+
 
 
 
@@ -18,10 +25,11 @@ function Food({
 
 
 return(
-    
+  
+  
+  <div id="flechas">	
 
-      <div>	
-      <h1>hola</h1>
+
       <div key={_id} id="containerdos">
 <div class="foodimage">
 	
@@ -29,17 +37,22 @@ return(
 	
 </div>
 
-	<h4 style={{marginTop:"25px", fontSize:"20px"}}>{name}</h4>
+	<h4 style={{marginTop:"15px", fontSize:"20px"}}>{name}</h4>
+  <img id="arrowleft" src={arrowleft} alt="" style={{marginRight:"15px"}}/>
+  <img id="close" src={close} alt="" style={{marginRight:"15px"}}/>
+  <img id="check" src={check} alt="" style={{marginRight:"15px"}}/>
+  <img id="arrowright" src={arrowright} alt=""/>
 
-  <img src={flechas} alt=""/>
+  
 
 
 <br/>
  
 </div>
+    
 </div>
      
-      
+     
 
      
      
