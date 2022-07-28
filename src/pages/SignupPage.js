7363a1157082c227c2fbc0c568f6689a3790b6a1
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-28 12:43:33 +0200

    añadido registro + pagina filtro
    añadido registro + pagina filtro

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -30,27 +30,57 @@ function SignupPage(props) {
   };
 
 	return (
-		<div className="SignupPage">
-			<h1>Sign Up</h1>
+		<div class="registration-form">
+        <form onSubmit={handleSignupSubmit}>
+            <div class="form-icon">
+                <span><i class="icon icon-user"></i></span>
+            </div>
+            <div class="form-group">
+                <input type="text" name="name" value={name} onChange={handleName} class="form-control item" id="username" placeholder="Username" />
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" value={password} onChange={handlePassword} class="form-control item" id="password" placeholder="Password" />
+            </div>
+            <div class="form-group">
+                <input type="text" name="email" value={email} onChange={handleEmail} class="form-control item" id="email" placeholder="Email" />
+            </div>
+            
+           
+            <div class="form-group">
+                <button type="submit" class="btn btn-block create-account">Create Account</button>
+            </div>
+        </form>
+        <div class="social-media">
+            <h5>Visit us</h5>
+            <div class="social-icons" sty>
+                <a target='_blank' style={{marginRight:"25px", textDecoration:"none"}} href="https://github.com/belendi1999">B</a>
+                <a target='_blank' style={{marginRight:"25px", textDecoration:"none"}} href="https://github.com/CristianMP04">C</a>
+                <a target='_blank' style={{marginRight:"25px", textDecoration:"none"}} href="https://github.com/NicMad4">N</a><br />
+				
+            </div>
+        </div>
+    </div>
+		// <div className="SignupPage">
+		// 	<h1>Sign Up</h1>
 
-			<form onSubmit={handleSignupSubmit}>
-				<label>Email:</label>
-				<input type="text" name="email" value={email} onChange={handleEmail} />
+		// 	<form onSubmit={handleSignupSubmit}>
+		// 		<label>Email:</label>
+		// 		<input type="text" name="email" value={email} onChange={handleEmail} />
 
-				<label>Password:</label>
-				<input type="password" name="password" value={password} onChange={handlePassword} />
+		// 		<label>Password:</label>
+		// 		<input type="password" name="password" value={password} onChange={handlePassword} />
 
-				<label>Name:</label>
-				<input type="text" name="name" value={name} onChange={handleName} />
+		// 		<label>Name:</label>
+		// 		<input type="text" name="name" value={name} onChange={handleName} />
 
-				<button type="submit">Sign Up</button>
-			</form>
+		// 		<button type="submit">Sign Up</button>
+		// 	</form>
 
-			{errorMessage && <p className="error-message">{errorMessage}</p>}
+		// 	{errorMessage && <p className="error-message">{errorMessage}</p>}
 
-			<p>Already have account?</p>
-			<Link to={'/login'}> Login</Link>
-		</div>
+		// 	<p>Already have account?</p>
+		// 	<Link to={'/login'}> Login</Link>
+		// </div>
 	);
 }
 
