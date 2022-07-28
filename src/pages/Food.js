a9c5a69b81a20b49f034598c5cf10fb0bc0db39f
Nico M <nico_16bcn@hotmail.com>
2022-07-28 13:07:47 +0200

    foodstindercards

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -16,46 +16,22 @@ function Food({
 return(
     
 
-      <div key={_id} id="container">	
+      <div key={_id} id="containerdos">	
+      
 	
-	<div class="product-details">
-		
-	<h1>{name}</h1>
+	<h3>{name}</h3>
 
-		
-			<p class="information">" Let's spread the joy , here is Christmas , the most awaited day of the year.Christmas Tree is what one need the most. Here is the correct tree which will enhance your Christmas.</p>
-
-		
-<div class="control">
 	
-	<button class="btn">
-	 <span class="price">$250</span>
-   <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
-   <span class="buy">Get now</span>
- </button>
+<div class="foodimage">
+	
+	<img  src={image} alt="" height="250px"/>
 	
 </div>
-		
-			
-</div>
-	
-<div class="product-image">
-	
-	<img  src={image} alt=""/>
-	
-
-<div class="info">
-	<h2> Description</h2>
-	<ul>
-		<li><strong>QUIERO <a target="_blank" href={link} style={{textDecoration:"none"}}>{restaurant}</a>!</strong></li>
-		<li><strong>Shade : </strong>Olive green</li>
-		<li><strong>Decoration: </strong>balls and bells</li>
-		
-		
-	</ul>
-</div>
-</div>
-
+<div class="tinder--buttons">
+<br/>
+    <button id="nope">nope</button>
+    <button id="love">love</button>
+  </div>
 </div>
      
       
